4f16413700727a2e85bd1597e92ba5c91af36d05
Nico-Diaz <ndiazruiz710@gmail.com>
2026-02-09 20:07:41 -0300

    Primer Commit

---
+++ src/data/links.js
@@ -0,0 +1,44 @@
+export const links = [
+  { 
+    title: "INSCRIPCIONES 2026", 
+    url: "../inscripciones", // URL Externa
+    type: "external",
+    color: "bg-[#a65d00]" 
+  },
+  { 
+    title: "SECRETARIA", 
+    url: "../secretaria", // URL Externa
+    type: "external",
+    color: "bg-gray-200" 
+  },
+  { 
+    title: "ADMINISTRACIÓN", 
+    url: "../administracion", // URL Externa
+    type: "external",
+    color: "bg-[#a65d00]" 
+  },
+  { 
+    title: "LIBRERIA", 
+    phone: "5492615876566", // Número directo
+    type: "whatsapp",
+    color: "bg-gray-200" 
+  },
+  { 
+    title: "KIOSKO", 
+    phone: "5492616950182", // Número directo
+    type: "whatsapp",
+    color: "bg-[#a65d00]" 
+  },
+  { 
+    title: "PASTORAL", 
+    phone: "5492616433469", // URL Externa
+    type: "external",
+    color: "bg-gray-200" 
+  },
+  { 
+    title: "¿DONDE ESTAMOS?", 
+    url: "https://maps.google.com/...", // Google Maps
+    type: "external",
+    color: "bg-[#a65d00]" 
+  },
+];
